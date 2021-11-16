9c32b69912a0dbdb2d95777cf327c1359c438f15
Radovan Prijić <rprijic3619rn@raf.rs>
2021-11-16 13:46:15 +0100

    Stavio sam kako bi trebale izgledati procedure i obicni upiti/viewovi u kodu.

--- src/queries/Query1.java
+++ src/queries/Query1.java
@@ -1,9 +1,48 @@
 package queries;
 
+import java.sql.*;
+
 public class Query1 extends AbstractQuery{
 
     @Override
     public void executeQuery() {
+        Connection myConnection = null;
+        Statement myStatement = null;
+        ResultSet myResultSet = null;
 
+        try {
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/adventureworks", "student", "student");
+            myStatement = myConnection.createStatement();
+            myResultSet = myStatement.executeQuery("select * from employees");
+
+            while(myResultSet.next()){
+                //npr. System.out.println(myResultSet.getString(indeks(krecu od 1) ili ime kolone));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(myConnection != null) {
+                try {
+                    myConnection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(myStatement != null) {
+                try {
+                    myStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(myResultSet != null) {
+                try {
+                    myResultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/queries/Query4.java
+++ src/queries/Query4.java
@@ -1,9 +1,52 @@
 package queries;
 
+import java.sql.*;
+
 public class Query4 extends AbstractQuery{
 
     @Override
     public void executeQuery() {
+        Connection myConnection = null;
+        CallableStatement myCall = null;
+        ResultSet myResultSet = null;
 
+        try {
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/adventureworks", "student", "student");
+            myCall = myConnection.prepareCall("{call procedure_name(?)}"); // svaki parametar je jedan upitnik
+            //myCall.setString(1, 'NC-17'), ovo je primer za prosledjivanje input parametra
+            //myCall.registerOutParameter(1, Types.INTEGER), primer za dobivanje out parametra
+            //int count = myCall.getInt(1), ovako onda dobijemo taj out parametar u promenljivu da mozemo da ga printamo
+            myCall.execute();
+            myResultSet = myCall.getResultSet();
+
+            while(myResultSet.next()){
+                //npr. System.out.println(myResultSet.getXXX(NESTO));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(myConnection != null) {
+                try {
+                    myConnection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(myCall != null) {
+                try {
+                    myCall.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(myResultSet != null) {
+                try {
+                    myResultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
